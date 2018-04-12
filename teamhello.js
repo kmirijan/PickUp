3d00aa4df662d7a2ef0db7e04889f399a26cc81f
shini <szhang93@ucsc.edu>
2018-04-11 18:15:58 -0700

    shini

--- teamhello.js
+++ teamhello.js
@@ -1,2 +1,6 @@
 console.log("Hello my name is Khachatur");
+<<<<<<< HEAD
 console.log("Hello my name is Shini");
+=======
+console.log("Hello my name is Rithu");
+>>>>>>> 191f9db12dba37f380e0871ee9a21154ce4600f4
