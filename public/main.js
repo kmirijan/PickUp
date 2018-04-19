4dfc001a48a0390865c3d38172399894dba843cf
shini <szhang93@ucsc.edu>
2018-04-18 17:25:37 -0700

    added input boxes

--- public/main.js
+++ public/main.js
@@ -1,1 +1,14 @@
-console.log("this works");
+//get form data
+var submit=()=>{
+	const event=document.getElementById("event").value;
+	const location=document.getElementById("location").value;
+
+	var response=document.getElementById("response");
+	response.textContent="You entered event:"+event+" location:"+location;
+};
+
+//main
+var b_submit=document.getElementById("submit");
+b_submit.addEventListener("click",submit);
+
+
