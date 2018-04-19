377e629045f0485fe9df4bb5ca0455d1ff7269a0
Alex Blumer <ablumer@ucsc.edu>
2018-04-19 12:21:26 -0700

    trying to use express package

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var MongoClient = require('mongodb').MongoClient, 
 	assert = require('assert'),
-	http = require('http')
+	app = require('express')
 	fs = require('fs');
 
-var url = 'mongodb://localhost:27017/games';
+var url = 'mongodb://pickup:cs115@ds251819.mlab.com:51819/pickup';
+
+
+app.get('/', function(req, res) {
+	MongoClient.connect(url, function (err, db) {
+		assert.equal(null, err);
+
+		// find all games
+		// return games
+	});
+})
+
+app.post('/', function(req, res) {
+	var game = {name: req.body.name, 
+		activity: req.body.activity,
+		loc: req.body.loc;
+	};
+
+	res.json(game);
+});
+
+app.listen(3000);
+
+
+// create a server using the http module
 
 http.createServer(function (req, res){
 	fs.readFile('home.html', function(err, data) {
@@ -12,6 +36,9 @@ http.createServer(function (req, res){
 	});
 }).listen(8080);
 
+
+// connects to the database, adds an object, and then prints the objects
+/*
 MongoClient.connect(url,function (err,db) {
 	assert.equal(null, err);
 	console.log("Connected successfully to server");
@@ -26,5 +53,5 @@ MongoClient.connect(url,function (err,db) {
 
 	db.close();
 });
-
+*/
 
