def798272f9ff8fd28267252e2036cef78ffa1cb
shini <szhang93@ucsc.edu>
2018-04-21 21:22:30 -0700

    webpack/babel working with node

---
+++ src/components/Head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+var Head=React.createClass({
+	render:()=>{
+      return(
+         <h1>Pickup</h1>
+      );
+   }
+});
+
+export default Head
---
+++ src/components/main.jsx
@@ -0,0 +1,15 @@
+var React=require('react');
+var ReactDOM=require('react-dom');
+
+class Main extends React.Component{
+	render(){
+		return{
+			<h1>Hello World!</h1>
+		}
+	}
+}
+
+
+
+
+
