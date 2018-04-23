19ccca091592560029faf1f8bbbb1bcb9ebfbf48
Alex Blumer <alpha.blumer@gmail.com>
2018-04-23 10:19:29 -0700

    Setting up webpack to run React

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+
+module.exports = 
+{
+	entry: path.join(path.resolve(__dirname, 'games.js')),
+	output:
+	{
+		path: path.resolve(__dirname, 'build'),
+		filename: 'games.build.js',
+	},
+
+
+	module: 
+	{
+		rules:
+		[
+			{
+				test: /\.(js|jsx)$/,
+				exclude /node_modules/,
+				use: 
+				[
+					'babel-loader'
+				],
+			},
+		],
+
+	},
+
+	resolve: 
+	{
+		extensions: ['.js', '.jsx'],
+	},
+};
