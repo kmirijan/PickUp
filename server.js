68d5ec34ac1d0feccabb3ceaf3118922c8b2b054
shini <szhang93@ucsc.edu>
2018-04-24 10:06:19 -0700

    finally

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require("express");
 const mongo=require("mongodb").MongoClient;
 const bodyParser=require("body-parser");
@@ -29,43 +28,3 @@ app.listen(port,()=>{
 
 
 
-=======
-const express = require('express');
-var bodyParser = require('body-parser');
-
-var {mongoose} = require('./db/mongoose');
-var {GameInfo} = require('./db/gameInfo');
-
-const port = process.env.PORT || 3000;
-var app = express();
-
-app.use(bodyParser.json());
-
-app.post('/', (req, res) => {
-	var gI = new GameInfo({
-		game: req.body.game,
-		location: req.body.location
-	});
-
-	gI.save().then((doc) => {
-		res.send(doc);
-	}, (e) => {
-		res.status(400).send();
-	});
-});
-
-app.get('/', (req, res) => {
-	GameInfo.find().then((gIs) => {
-		res.send({gIs});
-	}, (e) => {
-		res.staus(400).send();
-	});
-
-});
-
-app.listen(port, () => {
-	console.log(`Server is up: ${port}`);
-});
-
-module.exports = {app};
->>>>>>> e5a2011e4a5dd7e95a2428b867e82b4b820b252d
