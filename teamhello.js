fecc541600e7bd1116a0540086125278b057ad2e
szhang93 <36149018+szhang93@users.noreply.github.com>
2018-04-30 12:30:18 -0700

    Delete teamhello.js

--- teamhello.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello my name is Khachatur");
-console.log("Hello my name is Rithu");
-console.log("Hello my name is Rogelio");
-console.log("Hello my name is Shini");
-console.log("Hello my name is Rithu");
-console.log("Hello my name is Alex");
-console.log("I am just testing atom");
-console.log("testing branch");
