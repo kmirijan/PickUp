5fecc056f73973844630cac454f3bd03bb9e97d1
Rogelio Molina <rogelio.molina1@gmail.com>
2018-05-01 10:32:31 -0700

    cleaned up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ class NavBar extends React.Component {
     return (
       <div>
       <ul className="NavBar">
-       <li className="Nav"><NavLink activeClassName='active' className = 'pages' to={'/'}>Home</NavLink></li>
+       <li className="Nav"><NavLink activeClassName='active' className = 'pages' to={'/home'}>Home</NavLink></li>
        <li className="Nav"><NavLink activeClassName='active' className = 'pages' to={'/app'}>Create Game</NavLink></li>
        <li className="Nav"><NavLink activeClassName='active' className = 'pages' to={'/signin'}>Sign In</NavLink></li>
        <li className="Nav"><NavLink activeClassName='active' className = 'pages' to={'/signup'}>Sign Up</NavLink></li>
