8353d4d6a3606b4f60272e9f8de750a4b54810be
Alex Blumer <alpha.blumer@gmail.com>
2018-06-01 10:21:28 -0700

    Client-side validation for individual game creation

--- src/components/CreateGames.jsx
+++ src/components/CreateGames.jsx
@@ -28,42 +28,57 @@ export class CurrentGames extends React.Component{
     }
 
 
-
-
-    getName()
-    {
-        return this.props.user;
-    }
-
     addGame(event) {
-        $('#createSoloGames').collapse('hide');
         event.preventDefault();
-        let sport = this.refs.sport.getInput();
-        let name = this.getName();
-        let location = this.refs.location.getInput();
-        let isprivate = this.state.isprivate;
-        let coords = this.autocomplete.getPlace().geometry.location;
+        let place = this.autocomplete.getPlace();
+        if (place == undefined)
+        {
+            this.refs.location.setError("Please select a location from the dropdown menu");
+            return;
+        }
+        let coords = place.geometry.location;
+
         let id = Math.floor((Math.random()*(1 << 30))+1);
         let startTime = (new Date()).getTime();
-        let gameLength = this.refs.gameLength.getInput() * 60*60*1000; // expected length of game in milliseconds
+        let gameLength = parseFloat(this.refs.gameLength.getInput()) * 60*60*1000; // expected length of game in milliseconds
         let game = {
             id: id,
-            sport: sport,
-            name: name,
-            isprivate:isprivate,
-            location: location,
+            sport: this.refs.sport.getInput(),
+            name: this.props.user,
+            isprivate: this.state.isprivate,
+            location: this.refs.location.getInput(),
             user: this.props.user,
             lat: coords.lat(),
             lng: coords.lng(),
             startTime: startTime,
             gameLength: gameLength,
         };
-        console.log(game);
-        axios.post('/postgames', game)
-        axios.patch('/user:game', {uid: this.props.user, gid: game.id});
-        this.refs.sport.clear();
-        this.refs.location.clear();
-        this.refs.gameLength.clear();
+
+        if (this.gameValidate(game) == true)
+        {
+            $('#createSoloGames').collapse('hide');
+            console.log(game);
+            axios.post('/postgames', game)
+            axios.patch('/user:game', {uid: this.props.user, gid: game.id});
+            this.refs.sport.clear();
+            this.refs.location.clear();
+            this.refs.gameLength.clear();
+        }
+    }
+    gameValidate(game)
+    {
+        let isValid = true;
+        if (game.sport.trim() == "")
+        {
+            isValid = false;
+            this.refs.sport.setError("Please give a non-empty name");
+        }
+        if ( isNaN(game.gameLength) || game.gameLength < 0 )
+        {
+            isValid = false;
+            this.refs.gameLength.setError("Please input a non-negative number");
+        }
+        return isValid;
     }
     togglePrivate(){
       if(this.state.isprivate==false){
@@ -144,6 +159,19 @@ class GameInputField extends React.Component {
         this.refs.input.value = "";
     }
 
+    setError(err)
+    {
+        this.refs.errorField.innerHTML = err;
+    }
+
+    clearError()
+    {
+        if (this.refs.errorField.innerHTML != "")
+        {
+            this.refs.errorField.innerHTML = "";
+        }
+    }
+
     render()
     {
         return (
@@ -154,8 +182,10 @@ class GameInputField extends React.Component {
         				<span className="input-group-addon"></span>
           				<input required className='gameDetails form-control' type="text"  type="text"
                                 ref="input"
+                                onChange={this.clearError.bind(this)}
                                 {...this.props}/>
         			</div>
+                    <div className="errorField" ref="errorField"></div>
             	</div>
         </div>
         );
